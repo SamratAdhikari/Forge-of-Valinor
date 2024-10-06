3389925fbd24f97d1b313da92abe27d407b50ba0
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-10-06 15:10:30 +0545

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connect.db.js";
 import userRoutes from "./routes/controller.js";
+import path from "path";
 
 // initialization
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -15,6 +18,11 @@ await connectDB();
 
 // register routes
 app.use("/api/", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // port setup
 app.listen(PORT, () => {
